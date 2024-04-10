b17ee22dc5dd78b843b559876c8317f1f8247637
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-04-10 19:20:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -21,26 +21,26 @@ def fetch_response(user_input, model_name, image=None):
 
 def main():
     st.title("Google Generative AI Chatbot")
+    st.sidebar.header("Input")
+    user_input = st.sidebar.text_input("Enter your message here:")
+    uploaded_file = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+    model_selection = st.sidebar.selectbox("Select Model", ["Gemini 1", "Gemini 1.5"])
 
     # Display chat history
     for i, entry in enumerate(st.session_state['chat_history']):
-        if 'image' in entry:
-            st.write(f"**User:** {entry['user_input']}")
-            st.image(entry['image'], caption="Uploaded Image", use_column_width=True)
-        else:
-            st.write(f"**User:** {entry['user_input']}")
+        with st.expander(f"Conversation {i+1}"):
+            if 'image' in entry:
+                st.write("User uploaded an image")
+                st.image(entry['image'], caption="Uploaded Image", use_column_width=True)
+                st.write(f"User prompt: {entry['user_input']}")
+            else:
+                st.write(f"User: {entry['user_input']}")
+            st.write(f"Model: {entry['model_name']}")
+            st.write(f"Response: {entry['response']}")
 
-        st.write(f"**{entry['model_name']}:** {entry['response']}")
-        st.write("---")
-
-    # Get user input
-    user_input = st.text_area("Enter your message here:")
-    uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-    model_selection = st.selectbox("Select Model", ["Gemini 1", "Gemini 1.5"])
-
-    if st.button("Send"):
+    if st.sidebar.button("Send"):
         if model_selection == "Gemini 1":
-            model_name = "gemini-1.0-pro-latest"
+            model_name = "gemini-1.0-base-latest"
         else:
             model_name = "gemini-1.5-pro-latest"
 
@@ -60,6 +60,9 @@ def main():
                 'model_name': model_name,
                 'response': response
             })
+
+        st.subheader("Output")
+        st.text_area("", value=response, height=999, max_chars=None, key=None)
 
 if __name__ == "__main__":
     main()
